7f4e76cc7ab9fe38c1336e53bd2a8a003af32ede
sinach <ukaegbuosinachi15@gmail.com>
2023-08-22 18:15:57 +0100

    testing sth

--- run.py
+++ run.py
@@ -2,11 +2,6 @@ from flask import Flask
 from app.app import app
 import json
 from flask import request, jsonify
-import numpy as np
-from app.app import app
-from app import openai_utils
-from sklearn.metrics.pairwise import cosine_similarity
-from app.models import db, Symptom
 
 app = Flask(__name__)
 
@@ -16,3 +11,5 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+from app import routes, models    
