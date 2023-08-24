2cbaa30243cea3093c651be7acd778b58b86d7a5
sinach <ukaegbuosinachi15@gmail.com>
2023-08-24 17:31:45 +0100

    added flask_cors

--- app/app.py
+++ app/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 # from app.app import app
 # from config import Config
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+cors = CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
 
 from app import routes, models
