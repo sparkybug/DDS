4ac7da26c2d9a7b7cf7eb137aee6f25e59a03a75
sinach <ukaegbuosinachi15@gmail.com>
2023-08-24 16:29:43 +0100

    just now

--- app/app.py
+++ app/app.py
@@ -5,6 +5,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
 if __name__ == '__main__':
     app.run(debug=True)
 
