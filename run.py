ad25298f355e54fc1f9e91c2f366af810c19dff4
sinach <ukaegbuosinachi15@gmail.com>
2023-08-24 19:20:19 +0100

    performing magic

--- run.py
+++ run.py
@@ -13,7 +13,8 @@ def predict_disease():
     # Predict disease based on similarities
     similarities = routes.predict_diseases()
     predicted_disease = openai_utils.ranker(similarities)
-        
+
+    return similarities  
     return jsonify({'predicted_disease': predicted_disease}), 200
     # return routes.predict_diseases()
 
